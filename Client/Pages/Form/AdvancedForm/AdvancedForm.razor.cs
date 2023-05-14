c7db1c2d98f2013a7927b363960eb1f74685379c
WalidMoua <walidmou97@gmail.com>
2023-05-14 17:23:34 +0200

    Resolved conflict with branch merge

--- Client/Pages/Form/AdvancedForm/AdvancedForm.razor.cs
+++
@@ -1,9 +0,0 @@
-using Client.Models;
-
-namespace Client.Pages.Form
-{
-    public partial class AdvancedForm
-    {
-        private readonly AdvancedFormModel _model = new AdvancedFormModel();
-    }
-}
