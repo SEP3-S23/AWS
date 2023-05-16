6869e95676d17c73e08bd5e29413bfd98c745c12
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-05-16 20:14:45 +0200

    tried impl of forums and connecting not working -- need help

--- AWSApp/Client/Program.cs
+++ AWSApp/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
 using MudBlazor.Services;
+using Backend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+// Register the ForumService
+builder.Services.AddScoped<ForumService>();
 
 await builder.Build().RunAsync();
