f8e0bf5518d3405cfa3cf9c3504f08fed49220d9
f.depalma <325587@via.dk>
2023-05-22 17:54:59 +0200

    SSE fixed

---
+++ AWSApp/Client/wwwroot/js/sse.js
@@ -0,0 +1,13 @@
+﻿window.sse = {
+    setupSseConnection: function (dotNetRef, url) {
+        var eventSource = new EventSource(url);
+
+        eventSource.onmessage = function (event) {
+            dotNetRef.invokeMethodAsync('HandleSseMessage', event.data);
+        };
+
+        eventSource.onerror = function () {
+            dotNetRef.invokeMethodAsync('HandleSseError');
+        };
+    }
+};
---
+++ WSServer/src/main/java/com/group3/ws_server/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.group3.ws_server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Allow requests from any origin
+        configuration.addAllowedHeader("*"); // Allow all headers
+        configuration.addAllowedMethod("GET"); // Allow only GET requests
+        configuration.addAllowedMethod("OPTIONS");
+        configuration.addAllowedMethod("PUT");
+        configuration.addAllowedMethod("POST");
+        configuration.addAllowedMethod("DELETE");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
