613b7bc7b1af9e3691bf3d06527d43c8869240df
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-05-25 17:55:24 +0200

    --

---
+++ AWSApp/Shared/DTOs/CreateForumDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.DTOs;
+
+public class CreateForumDto
+{
+    private string ForumName;
+
+    private string ForumDescription;
+
+    private string ForumCategory;
+
+    public CreateForumDto(string name, string description, string cagetory)
+    {
+        ForumName = name;
+        ForumDescription = description;
+        ForumCategory = cagetory;
+    }
+
+}
